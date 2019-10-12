47ac4b5b6b944f96ddcda97da11e9d95a78eecb6
psr <hello@prabhjotrai.com>
2019-10-12 02:15:15 -0500

    feat: create match fetcher file to fetch all match ids

---
+++ data_dump/match_fetcher.py
@@ -0,0 +1,25 @@
+import requests, time, logging
+from constants.constants import GET_MATCH_HISTORY, KEY
+
+logging.basicConfig(filename='match_fetcher.log', level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
+
+while True:
+    logging.info(f'Getting match history')
+    try:
+        response = requests.get(GET_MATCH_HISTORY, params={'key': KEY})
+        if response.status_code == 200:
+            try:
+                response_json = response.json()
+                f = open('matches.log', 'a+')
+                f.write("\n")
+                matches = response_json['result']['matches']
+                f.write("\n".join(map(lambda t: str(t['match_id']), matches)))
+                f.close()
+                logging.info(f'Successfully written {len(matches)} records')
+            except ValueError as v:
+                logging.error(f'Decoding JSON has failed: {str(v)}')
+        else:
+            logging.error(f'Response status code: {response.status_code}')
+    except Exception as e:
+        logging.error(f'Error occurred {str(e)}, retrying')
+    time.sleep(2)
