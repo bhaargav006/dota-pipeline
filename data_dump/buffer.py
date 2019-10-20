3c40389fda87962627e58950bab1c3ddb01a72b0
Bhaargav Sriraman <bhaargavsriraman@Bhaargavs-MacBook-Pro.local>
2019-10-19 22:32:46 -0500

    Feat: Adding to file and logging the result

---
+++ data_dump/buffer.py
@@ -0,0 +1,29 @@
+import time, queue
+from match_details_fetcher import getMatchDetails
+
+matchIDs = queue.Queue()
+
+
+def operateQueue():
+    logfile = open("access-log", "r")
+    loglines = follow(logfile)
+    for line in loglines:
+        matchIDs.put(line)
+        yield matchIDs.get()
+
+
+def follow(thefile):
+    thefile.seek(0, 0)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+if __name__ == '__main__':
+    for value in operateQueue():
+        print(f'calling match details with {value}')
+        if getMatchDetails(value) != 200:
+            matchIDs.put(value)
