32618ad2e808e0e914f1808d09c18d071fbc24b5
psr <hello@prabhjotrai.com>
2019-10-19 21:01:00 -0500

    feat: add serial match fetcher

--- data_dump/serial_match_fetcher.py
+++ data_dump/serial_match_fetcher.py
@@ -1,21 +1,27 @@
 import requests, time, logging
 from constants.constants import GET_MATCH_HISTORY, KEY_1
 
-logging.basicConfig(filename='match_fetcher.log', level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
+logging.basicConfig(filename='serial_match_fetcher.log', level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
+last_match_id = None
 
 while True:
     logging.info(f'Getting match history')
+    params = {'key': KEY_1, 'skill': 3, 'min_players': 10}
     try:
-        response = requests.get(GET_MATCH_HISTORY, params={'key': KEY_1})
+        response = requests.get(GET_MATCH_HISTORY, params=params)
         if response.status_code == 200:
             try:
                 response_json = response.json()
-                f = open('matches.log', 'a+')
-                f.write("\n")
                 matches = response_json['result']['matches']
-                f.write("\n".join(map(lambda t: str(t['match_id']), matches)))
-                f.close()
+                match_ids = [match['match_id'] for match in matches]
+                match_ids.sort()
+                f = open('serial_matches.log', 'a+')
+                f.write("\n")
+                params['start_at_match_id'] = last_match_id
+                last_match_id = match_ids[0]
                 logging.info(f'Successfully written {len(matches)} records')
+                f.write("\n".join(map(lambda t: str(t), match_ids)))
+                f.close()
             except ValueError as v:
                 logging.error(f'Decoding JSON has failed: {str(v)}')
         else:
