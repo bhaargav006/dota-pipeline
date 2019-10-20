b49108aca18e6fd4f27e9747dc292cd025fce0e0
psr <hello@prabhjotrai.com>
2019-10-20 03:10:10 -0500

    feat: query by last sequence number

--- data_dump/serial_match_fetcher.py
+++ data_dump/serial_match_fetcher.py
@@ -1,23 +1,25 @@
 import requests, time, logging
-from constants.constants import GET_MATCH_HISTORY, KEY_1, DATA_ROOT, LOG_ROOT
+from constants.constants import GET_MATCH_HISTORY, KEY_4, DATA_ROOT, LOG_ROOT, GET_MATCH_HISTORY_BY_SEQ_NUM
 
 logging.basicConfig(filename=LOG_ROOT+'serial_match_fetcher.log', level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
-last_match_id = None
-params = {'key': KEY_1, 'skill': 3, 'min_players': 10}
+last_match_sequence = 4137617145
+params = {'key': KEY_4, 'skill': 3, 'min_players': 10, 'start_at_match_seq_num': last_match_sequence}
 
 while True:
     logging.info(f'Getting match history')
     
     try:
-        response = requests.get(GET_MATCH_HISTORY, params=params)
+        response = requests.get(GET_MATCH_HISTORY_BY_SEQ_NUM, params=params)
         if response.status_code == 200:
             try:
                 response_json = response.json()
                 matches = response_json['result']['matches']
                 match_ids = [match['match_id'] for match in matches]
-                match_ids.sort()
-                last_match_id = match_ids[0]
-                params['start_at_match_id'] = last_match_id
+                sequence_ids = [match['match_seq_num'] for match in matches]
+                sequence_ids.sort()
+                last_match_sequence = sequence_ids[-1]
+                params['start_at_match_seq_num'] = last_match_sequence
+                print(match_ids)
                 f = open(DATA_ROOT+'serial_matches.log', 'a+')
                 f.write("\n")
                 f.write("\n".join(map(lambda t: str(t), match_ids)))
