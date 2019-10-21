1aed3abab2dc6e0f2c13e279577711573616a789
psr <hello@prabhjotrai.com>
2019-10-20 22:39:05 -0500

    feat: more final changes to serial match fetcher code

--- data_dump/serial_match_fetcher.py
+++ data_dump/serial_match_fetcher.py
@@ -1,12 +1,18 @@
-import requests, time, logging
+import requests, time, logging, datetime
 from constants.constants import KEY_4, DATA_ROOT, LOG_ROOT, GET_MATCH_HISTORY_BY_SEQ_NUM
 
 logging.basicConfig(filename=LOG_ROOT+'serial_match_fetcher.log', level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
 last_match_sequence = 4137117145
 params = {'key': KEY_4, 'skill': 3, 'min_players': 10, 'start_at_match_seq_num': last_match_sequence}
+DRY_RUN = True
+SLEEP_TIME = 0.5
 
+
+MODE = 'DRY MODE' if DRY_RUN else 'LIVE MODE'
+logging.info(f'SERIAL MATCH FETCHER RUNNING IN {MODE}')
 while True:
     logging.info(f'Getting match history')
+    t1 = datetime.datetime.now()
     try:
         response = requests.get(GET_MATCH_HISTORY_BY_SEQ_NUM, params=params)
         if response.status_code == 200:
@@ -18,11 +24,14 @@ while True:
                 sequence_ids.sort()
                 last_match_sequence = sequence_ids[-1]
                 params['start_at_match_seq_num'] = last_match_sequence
-                f = open(DATA_ROOT+'serial_matches.log', 'a+')
-                f.write("\n")
-                f.write("\n".join(map(lambda t: str(t), match_ids)))
-                f.close()
-                logging.info(f'Successfully written {len(matches)} records')
+                if not DRY_RUN:
+                    f = open(DATA_ROOT+'serial_matches.log', 'a+')
+                    f.write("\n")
+                    f.write("\n".join(map(lambda t: str(t), match_ids)))
+                    f.close()
+                    logging.info(f'Successfully written {len(matches)} records')
+                else:
+                    print(f'Running in DRY mode, data that would be written: {" ".join(map(lambda t: str(t), match_ids))}')
             except ValueError as v:
                 logging.error(f'Decoding JSON has failed: {str(v)}')
             except Exception as e:
@@ -31,4 +40,6 @@ while True:
             logging.error(f'Response status code: {response.status_code}')
     except Exception as e:
         logging.error(f'Error occurred {str(e)}, retrying')
-    time.sleep(2)
+    t2 = datetime.datetime.now()
+    logging.info(f'Time taken for the loop: {t2 - t1}')
+    time.sleep(SLEEP_TIME)
