3f7be16a553caa2a83bf6a2e14abbd1591603f52
psr <hello@prabhjotrai.com>
2019-10-24 00:13:57 -0500

    fix: some changes to unique extractor code

--- data_dump/unique_extractor.py
+++ data_dump/unique_extractor.py
@@ -4,9 +4,8 @@ from library.constants import DATA_ROOT, LOG_ROOT
 from google.cloud import pubsub_v1
 
 logging.basicConfig(filename=LOG_ROOT+'unique_extractor.log', level=logging.DEBUG, format='%(levelname)s:%(asctime)s %(message)s')
-logging.info(f'Extracting unique values')
 
-matchSet = set()
+match_list = set()
 
 project_id = 'big-data-arch-and-engineering'
 topic_name = 'match-queue'
@@ -16,27 +15,27 @@ topic_path = publisher.topic_path(project_id, topic_name)
 
 futures = dict()
 
-try:
-    ts = calendar.timegm(time.gmtime())
-    os.rename(DATA_ROOT + 'serial_matches.log', DATA_ROOT + 'serial_matches_' + str(ts) + '.log') 
+ts = str(calendar.timegm(time.gmtime()))
 
-    matchList = []
-    with open(DATA_ROOT + 'serial_matches_' + str(ts) + '.log') as f:
-        matchList = f.readlines()
+logging.info(f'Process ID: {ts}: Extracting unique values')
+os.rename(DATA_ROOT + 'serial_matches.log', DATA_ROOT + 'serial_matches_' + ts + '.log')
 
-    matchSet = set(matchList)
+with open(DATA_ROOT + 'serial_matches_' + ts + '.log') as f:
+    match_list = f.readlines()
 
-    with open(DATA_ROOT + 'unique-matches.txt',"a") as f:
-        for item in matchSet:
+match_list = list(filter(None, [v.rstrip() for v in match_list]))
+
+with open(DATA_ROOT + f'unique-matches.txt', "a") as f:
+    for item in match_list:
+        try:
             f.write("%s\n" % item)
-
             data = item
             publisher.publish(topic_path, data=data.encode('utf-8'))
-            logging.info(f'Published message ' + str(item) + ' to queue')
-    
-except Exception as e:
-    logging.error(f'Error occurred {str(e)}, adding match ids to original file')
-    with open(DATA_ROOT + 'serial_matches.log','a') as f:
-        for item in matchSet:
-            f.write("%s\n" % item)
-    logging.debug(f'Added match ids to original file')
+            logging.info(f'Process ID: {ts}: Published message {str(item)} to queue')
+        except Exception as e:
+            logging.error(f'Error occurred {str(e)}, adding remaining match ids to original file')
+            with open(DATA_ROOT + 'serial_matches_failed_transactions.log', 'a') as t:
+                t.write("%s\n" % item)
+            logging.debug(f'Added match id to the original file')
+
+logging.info(f'Process ID: {ts}: Process completed successfully.')
