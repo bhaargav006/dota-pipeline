fa780341d9aad2af3038efc1a6967d9078c7d025
psr <hello@prabhjotrai.com>
2019-10-25 00:03:14 -0500

    feat: add cronjob file

---
+++ data_dump/make_batch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+PROJECT_ROOT="$HOME/raiprabh/dota-pipeline"
+
+cd "$PROJECT_ROOT/data_dump" || return
+PROCESS_NAME=$(date +%s)
+LOG_FILE="$PROJECT_ROOT/log/make_batch.log"
+MATCH_FILE="$PROJECT_ROOT/data/serial_matches.log"
+DESTINATION_MATCH_FILE="$PROJECT_ROOT/data/serial_matches_$PROCESS_NAME.log"
+if [ -f $MATCH_FILE ]
+then
+  echo "Info: Process: $PROCESS_NAME: File serial_matches.log found, renaming the file to serial_matches_$PROCESS_NAME" >> $LOG_FILE
+  mv "$MATCH_FILE" "$DESTINATION_MATCH_FILE"
+  # activating the virtual environment
+  source "$HOME/env/bin/activate"
+  # running the script
+  GOOGLE_APPLICATION_CREDENTIALS=big-data-arch-and-engineering-331470f88904.json python "unique_extractor.py" "$PROCESS_NAME"
+else
+  echo "Error: Process: $PROCESS_NAME: File not found!" >> $LOG_FILE
+fi
