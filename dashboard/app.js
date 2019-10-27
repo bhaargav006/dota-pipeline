7c7a6aa1b43f1ad775914429bb1be9b0de0f6e1b
Suresh <suresh.siddharth@gmail.com>
2019-10-27 17:24:33 -0500

    Corrects typo

--- dashboard/app.js
+++ dashboard/app.js
@@ -6,7 +6,6 @@ DATABASE_URL = '54.245.218.63'
 console.log('Node.js code is now running!')
 var client = new faunadb.Client({ secret: 'secret', domain: DATABASE_URL, scheme: 'http', port: '8443'});
 
-<<<<<<< HEAD
 // ## WIP
 // var startTime = Date.now()
 // client.query(q.Max(q.Match(q.Index('matches_duration')))).then((ret) => console.log(ret))
@@ -24,79 +23,7 @@ client.query(
     (ret) => console.log('Match Duration: ' + ret['data']['result']['duration'] + ' and time to fetch: ' + (Date.now() - startTime) + 'ms')
 )
 
-// Mean match duraion
-=======
-// ## Create index for duration of matches
-// client.query(
-//     q.CreateIndex(
-//         {
-//             name: 'matches_raw_duration', 
-//             source: q.Collection('matches_raw'), 
-//             terms: [], 
-//             values: [{ field: ['data', 'result', 'duration'] }]
-//         }
-//     )
-// ).then(
-//     (ret) => console.log(ret)
-// )
-
-// ## Create index for provenance of api_call_type
-client.query(
-    q.CreateIndex(
-        {
-            name: 'matches_raw_prov_api_duration', 
-            source: q.Collection('matches_raw'), 
-            terms: [], 
-            values: [{ field: ['data', 'provenance', 'dataFetchStage', 'apiCallDuration'] }]
-        }
-    )
-).then(
-    (ret) => console.log(ret),
-    (err) => console.log(err)
-)
-
-// ## Create index for average first blood
-// client.query(
-//     q.CreateIndex(
-//         {
-//             name: 'matches_raw_fb_time', 
-//             source: q.Collection('matches_raw'), 
-//             terms: [], 
-//             values: [{ field: ['data', 'results', 'first_blood_time'] }]
-//         }
-//     )
-// ).then(
-//     (ret) => console.log(ret),
-//     (err) => console.log(err)
-// )
-
-// ## Raw Data Count index
-// client.query(
-//     q.CreateIndex({
-//         name: 'all_raw_matches',
-//         source: q.Collection('matches_raw')
-//     })
-// ).then((ret) => console.log(ret))
-
-// ## Query Match Duration
-// var startTime = Date.now()
-// client.query(
-//     q.Get(
-//         q.Ref(
-//             q.Collection('matches'), 
-//             '4931268777'
-//         )
-//     )
-// ).then(
-    // (ret) => console.log('Match Duration: ' + ret['data']['result']['duration'] + ' and time to fetch: ' + (Date.now() - startTime) + 'ms')
-// )
-
-// ## WIP
-// var startTime = Date.now()
-// client.query(q.Max(q.Match(q.Index('matches_duration')))).then((ret) => console.log(ret))
-
-// ## Mean match duraion
->>>>>>> Adds alert processor and extra keys
+// Mean match duration
 client.query(
     q.Mean(
         q.Match(
@@ -107,11 +34,7 @@ client.query(
     (ret) => console.log('Mean game duration: ' + (ret / 60).toFixed(2) + ' minutes')
 )
 
-<<<<<<< HEAD
 // Max match duraion
-=======
-// ## Max match duraion
->>>>>>> Adds alert processor and extra keys
 client.query(
     q.Max(
         q.Match(
@@ -122,11 +45,7 @@ client.query(
     (ret) => console.log('Max game duration: ' + (ret / 60).toFixed(2) + ' minutes')
 )
 
-<<<<<<< HEAD
 // Max match duraion
-=======
-// ## Max match duraion
->>>>>>> Adds alert processor and extra keys
 client.query(
     q.Min(
         q.Match(
@@ -137,11 +56,7 @@ client.query(
     (ret) => console.log('Min game duration: ' + (ret / 60).toFixed(2) + ' minutes')
 )
 
-<<<<<<< HEAD
 // Mean First Blood time
-=======
-// ## Mean First Blood time
->>>>>>> Adds alert processor and extra keys
 client.query(
     q.Mean(
         q.Match(
@@ -152,20 +67,7 @@ client.query(
     (ret) => console.log('Mean FB Time: ' + (ret / 60).toFixed(2) + ' minutes')
 )
 
-<<<<<<< HEAD
 // Count of records
-=======
-// client.query(
-//     q.Paginate(
-//         q.Match(
-//             q.Index('matches_duration')
-//         )    
-//     )
-// )
-// .then((ret) => console.log(ret))
-
-// ## Count of records
->>>>>>> Adds alert processor and extra keys
 var startTime = Date.now()
 client.query(
     q.Count(
@@ -176,8 +78,4 @@ client.query(
 ).then(
     (ret) => console.log('Number of matches in DB: ' + ret + ' and time to fetch: ' + (Date.now() - startTime) + 'ms at ' + new Date().toISOString()),
     (err) => console.log(err)
-<<<<<<< HEAD
 )
-=======
-)
->>>>>>> Adds alert processor and extra keys
