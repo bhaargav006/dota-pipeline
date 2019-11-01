d3954c54abfcc09ccdfd2df660675aeecd27eb3d
Bhaargav Sriraman <bhaargavsriraman@gmail.com>
2019-11-01 14:23:21 -0500

    Feat: Adding logging to user query functions

--- data_dump/user_queries.py
+++ data_dump/user_queries.py
@@ -12,7 +12,8 @@ logging.basicConfig(filename=LOG_ROOT + 'user_queries.log', level=logging.DEBUG,
                     format='%(levelname)s:%(asctime)s %(message)s')
 
 
-def queryHeroPair(hero_id, n):
+def getTopHeroPairs(hero_id, n):
+    logging.info(f'Query: Fetching top {n} partners for hero: {hero_id}')
     key_list = []
     id_list = []
     for k in range(1, hero_id):
@@ -33,44 +34,61 @@ def queryHeroPair(hero_id, n):
                 key_list
             )
         )
+        logging.debug('Finished querying the hero_pair collection successfully')
+
+        hero_team_list = []
+        for i in range(0, len(hero_data_list)):
+            hero_team_dictionary = {'partner_id': id_list[i]}
+            if hero_data_list[i]['data']['games'] > 0:
+                hero_team_dictionary['win_rate'] = format(
+                    hero_data_list[i]['data']['wins'] / hero_data_list[i]['data']['games'], '.4f')
+            else:
+                hero_team_dictionary['win_rate'] = '0.0000'
+            hero_team_list.append(hero_team_dictionary)
+
+        hero_team_list = sorted(hero_team_list, key=lambda i: i['win_rate'], reverse=True)
+
+        for i in range(0, n):
+            print(hero_team_list[i]['partner_id'])
+            print(hero_team_list[i]['win_rate'])
+
+        logging.info('Returning from the function getTopHeroPairs')
+        return hero_team_list[0: n]
 
     except Exception as e:
-        print(e)
-
-    hero_team_list = []
-    for i in range(0, len(hero_data_list)):
-        hero_team_dictionary = {'partner_id': id_list[i]}
-        if hero_data_list[i]['data']['games'] > 0:
-            hero_team_dictionary['win_rate'] = format(
-                hero_data_list[i]['data']['wins'] / hero_data_list[i]['data']['games'], '.4f')
-        else:
-            hero_team_dictionary['win_rate'] = '0.0000'
-        hero_team_list.append(hero_team_dictionary)
-
-    hero_team_list = sorted(hero_team_list, key=lambda i: i['win_rate'], reverse=True)
-
-    for i in range(0, n):
-        print(hero_team_list[i]['partner_id'])
-        print(hero_team_list[i]['win_rate'])
+        logging.error(e)
+        logging.error('Could not fetch from hero_pairs collection')
 
 
-def queryItemList(hero_id, n):
-    item_data = client.query(
-        q.get(
-            q.ref(q.collection("heroes"),
-                  hero_id
-                  )
+def getTopItems(hero_id, n):
+    logging.info(f'Query: Fetching top {n} items for hero: {hero_id}')
+    try:
+        item_data = client.query(
+            q.get(
+                q.ref(q.collection("heroes"),
+                      hero_id
+                      )
+            )
         )
-    )
-    item_data = item_data['data']['items']
-    item_list = []
-    for i in range(1, len(item_data)):
-        item_dict = {'item_id': i, 'item_count': item_data[i]}
-        item_list.append(item_dict)
 
-    item_list = sorted(item_list, key=lambda i: i['item_count'], reverse=True)
+        logging.debug('Finished querying the heroes collection successfully')
 
-    for i in range(0, n):
-        print(item_list[i]['item_id'])
-        print(item_list[i]['item_count'])
+        item_data = item_data['data']['items']
+        item_list = []
+        for i in range(1, len(item_data)):
+            item_dict = {'item_id': i, 'item_count': item_data[i]}
+            item_list.append(item_dict)
+
+        item_list = sorted(item_list, key=lambda i: i['item_count'], reverse=True)
+
+        for i in range(0, n):
+            print(item_list[i]['item_id'])
+            print(item_list[i]['item_count'])
+
+        logging.info('Returning from the function getTopItems')
+        return item_list[0:n]
+
+    except Exception as e:
+        logging.error(e)
+        logging.error('Could not fetch from heroes collection')
 
