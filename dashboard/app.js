cf12b7943473303a5b195343ae8c602b87df1cd8
Suresh <suresh.siddharth@gmail.com>
2019-11-05 18:51:35 -0600

    Removes conflict code

--- dashboard/app.js
+++ dashboard/app.js
@@ -7,30 +7,9 @@ DATABASE_URL = '54.245.218.63'
 console.log('Node.js code is now running!')
 var client = new faunadb.Client({ secret: 'secret', domain: DATABASE_URL, scheme: 'http', port: '8443', timeout: 500});
 
-<<<<<<< HEAD
 var hero_map = new Map([[1,"antimage"], [2,"axe"], [3,"bane"], [4,"bloodseeker"], [5,"crystal maiden"], [6,"drow ranger"], [7,"earthshaker"], [8,"juggernaut"], [9,"mirana"], [11,"nevermore"], [10,"morphling"], [12,"phantom lancer"], [13,"puck"], [14,"pudge"], [15,"razor"], [16,"sand king"], [17,"storm spirit"], [18,"sven"], [19,"tiny"], [20,"vengefulspirit"], [21,"windrunner"], [22,"zuus"], [23,"kunkka"], [25,"lina"], [31,"lich"], [26,"lion"], [27,"shadow shaman"], [28,"slardar"], [29,"tidehunter"], [30,"witch doctor"], [32,"riki"], [33,"enigma"], [34,"tinker"], [35,"sniper"], [36,"necrolyte"], [37,"warlock"], [38,"beastmaster"], [39,"queenofpain"], [40,"venomancer"], [41,"faceless void"], [42,"skeleton king"], [43,"death prophet"], [44,"phantom assassin"], [45,"pugna"], [46,"templar assassin"], [47,"viper"], [48,"luna"], [49,"dragon knight"], [50,"dazzle"], [51,"rattletrap"], [52,"leshrac"], [53,"furion"], [54,"life stealer"], [55,"dark seer"], [56,"clinkz"], [57,"omniknight"], [58,"enchantress"], [59,"huskar"], [60,"night stalker"], [61,"broodmother"], [62,"bounty hunter"], [63,"weaver"], [64,"jakiro"], [65,"batrider"], [66,"chen"], [67,"spectre"], [69,"doom bringer"], [68,"ancient apparition"], [70,"ursa"], [71,"spirit breaker"], [72,"gyrocopter"], [73,"alchemist"], [74,"invoker"], [75,"silencer"], [76,"obsidian destroyer"], [77,"lycan"], [78,"brewmaster"], [79,"shadow demon"], [80,"lone druid"], [81,"chaos knight"], [82,"meepo"], [83,"treant"], [84,"ogre magi"], [85,"undying"], [86,"rubick"], [87,"disruptor"], [88,"nyx assassin"], [89,"naga siren"], [90,"keeper of the light"], [91,"wisp"], [92,"visage"], [93,"slark"], [94,"medusa"], [95,"troll warlord"], [96,"centaur"], [97,"magnataur"], [98,"shredder"], [99,"bristleback"], [100,"tusk"], [101,"skywrath mage"], [102,"abaddon"], [103,"elder titan"], [104,"legion commander"], [106,"ember spirit"], [107,"earth spirit"], [109,"terrorblade"], [110,"phoenix"], [111,"oracle"], [105,"techies"], [112,"winter wyvern"], [113,"arc warden"], [108,"abyssal underlord"], [114,"monkey king"], [120,"pangolier"], [119,"dark willow"], [121,"grimstroke"], [129,"mars"]])
-
 var item_map = new Map([[1,"blink"], [2,"blades of attack"], [3,"broadsword"], [4,"chainmail"], [5,"claymore"], [6,"helm of iron will"], [7,"javelin"], [8,"mithril hammer"], [9,"platemail"], [10,"quarterstaff"], [11,"quelling blade"], [237,"faerie fire"], [265,"infused raindrop"], [244,"wind lace"], [12,"ring of protection"], [182,"stout shield"], [246,"recipe moon shard"], [247,"moon shard"], [13,"gauntlets"], [14,"slippers"], [15,"mantle"], [16,"branches"], [17,"belt of strength"], [18,"boots of elves"], [19,"robe"], [20,"circlet"], [261,"crown"], [21,"ogre axe"], [22,"blade of alacrity"], [23,"staff of wizardry"], [24,"ultimate orb"], [25,"gloves"], [26,"lifesteal"], [27,"ring of regen"], [279,"ring of tarrasque"], [28,"sobi mask"], [29,"boots"], [30,"gem"], [31,"cloak"], [32,"talisman of evasion"], [33,"cheese"], [34,"magic stick"], [35,"recipe magic wand"], [36,"magic wand"], [37,"ghost"], [38,"clarity"], [216,"enchanted mango"], [39,"flask"], [40,"dust"], [41,"bottle"], [42,"ward observer"], [43,"ward sentry"], [217,"recipe ward dispenser"], [218,"ward dispenser"], [44,"tango"], [241,"tango single"], [45,"courier"], [46,"tpscroll"], [47,"recipe travel boots"], [219,"recipe travel boots 2"], [48,"travel boots"], [220,"travel boots 2"], [49,"recipe phase boots"], [50,"phase boots"], [51,"demon edge"], [52,"eagle"], [53,"reaver"], [54,"relic"], [55,"hyperstone"], [56,"ring of health"], [57,"void stone"], [58,"mystic staff"], [59,"energy booster"], [60,"point booster"], [61,"vitality booster"], [62,"recipe power treads"], [63,"power treads"], [64,"recipe hand of midas"], [65,"hand of midas"], [66,"recipe oblivion staff"], [67,"oblivion staff"], [68,"recipe pers"], [69,"pers"], [70,"recipe poor mans shield"], [71,"poor mans shield"], [72,"recipe bracer"], [73,"bracer"], [74,"recipe wraith band"], [75,"wraith band"], [76,"recipe null talisman"], [77,"null talisman"], [78,"recipe mekansm"], [79,"mekansm"], [80,"recipe vladmir"], [81,"vladmir"], [85,"recipe buckler"], [86,"buckler"], [87,"recipe ring of basilius"], [88,"ring of basilius"], [268,"recipe holy locket"], [269,"holy locket"], [89,"recipe pipe"], [90,"pipe"], [91,"recipe urn of shadows"], [92,"urn of shadows"], [93,"recipe headdress"], [94,"headdress"], [95,"recipe sheepstick"], [96,"sheepstick"], [97,"recipe orchid"], [98,"orchid"], [245,"recipe bloodthorn"], [250,"bloodthorn"], [251,"recipe echo sabre"], [252,"echo sabre"], [99,"recipe cyclone"], [100,"cyclone"], [233,"recipe aether lens"], [232,"aether lens"], [101,"recipe force staff"], [102,"force staff"], [262,"recipe hurricane pike"], [263,"hurricane pike"], [103,"recipe dagon"], [197,"recipe dagon 2"], [198,"recipe dagon 3"], [199,"recipe dagon 4"], [200,"recipe dagon 5"], [104,"dagon"], [201,"dagon 2"], [202,"dagon 3"], [203,"dagon 4"], [204,"dagon 5"], [105,"recipe necronomicon"], [191,"recipe necronomicon 2"], [192,"recipe necronomicon 3"], [106,"necronomicon"], [193,"necronomicon 2"], [194,"necronomicon 3"], [107,"recipe ultimate scepter"], [108,"ultimate scepter"], [270,"recipe ultimate scepter 2"], [271,"ultimate scepter 2"], [109,"recipe refresher"], [110,"refresher"], [111,"recipe assault"], [112,"assault"], [113,"recipe heart"], [114,"heart"], [115,"recipe black king bar"], [116,"black king bar"], [117,"aegis"], [118,"recipe shivas guard"], [119,"shivas guard"], [120,"recipe bloodstone"], [121,"bloodstone"], [122,"recipe sphere"], [123,"sphere"], [221,"recipe lotus orb"], [226,"lotus orb"], [222,"recipe meteor hammer"], [223,"meteor hammer"], [224,"recipe nullifier"], [225,"nullifier"], [255,"recipe aeon disk"], [256,"aeon disk"], [258,"recipe kaya"], [259,"kaya"], [275,"trident"], [276,"combo breaker"], [260,"refresher shard"], [266,"recipe spirit vessel"], [267,"spirit vessel"], [124,"recipe vanguard"], [125,"vanguard"], [243,"recipe crimson guard"], [242,"crimson guard"], [126,"recipe blade mail"], [127,"blade mail"], [128,"recipe soul booster"], [129,"soul booster"], [130,"recipe hood of defiance"], [131,"hood of defiance"], [132,"recipe rapier"], [133,"rapier"], [134,"recipe monkey king bar"], [135,"monkey king bar"], [136,"recipe radiance"], [137,"radiance"], [138,"recipe butterfly"], [139,"butterfly"], [140,"recipe greater crit"], [141,"greater crit"], [142,"recipe basher"], [143,"basher"], [144,"recipe bfury"], [145,"bfury"], [146,"recipe manta"], [147,"manta"], [148,"recipe lesser crit"], [149,"lesser crit"], [234,"recipe dragon lance"], [236,"dragon lance"], [150,"recipe armlet"], [151,"armlet"], [183,"recipe invis sword"], [152,"invis sword"], [248,"recipe silver edge"], [249,"silver edge"], [153,"recipe sange and yasha"], [154,"sange and yasha"], [272,"recipe kaya and sange"], [273,"kaya and sange"], [274,"recipe yasha and kaya"], [277,"yasha and kaya"], [155,"recipe satanic"], [156,"satanic"], [157,"recipe mjollnir"], [158,"mjollnir"], [159,"recipe skadi"], [160,"skadi"], [161,"recipe sange"], [162,"sange"], [163,"recipe helm of the dominator"], [164,"helm of the dominator"], [165,"recipe maelstrom"], [166,"maelstrom"], [167,"recipe desolator"], [168,"desolator"], [169,"recipe yasha"], [170,"yasha"], [171,"recipe mask of madness"], [172,"mask of madness"], [173,"recipe diffusal blade"], [174,"diffusal blade"], [175,"recipe ethereal blade"], [176,"ethereal blade"], [177,"recipe soul ring"], [178,"soul ring"], [179,"recipe arcane boots"], [180,"arcane boots"], [228,"recipe octarine core"], [235,"octarine core"], [181,"orb of venom"], [240,"blight stone"], [184,"recipe ancient janggo"], [185,"ancient janggo"], [186,"recipe medallion of courage"], [187,"medallion of courage"], [227,"recipe solar crest"], [229,"solar crest"], [188,"smoke of deceit"], [257,"tome of knowledge"], [189,"recipe veil of discord"], [190,"veil of discord"], [230,"recipe guardian greaves"], [231,"guardian greaves"], [205,"recipe rod of atos"], [206,"rod of atos"], [238,"recipe iron talon"], [239,"iron talon"], [207,"recipe abyssal blade"], [208,"abyssal blade"], [209,"recipe heavens halberd"], [210,"heavens halberd"], [211,"recipe ring of aquila"], [212,"ring of aquila"], [213,"recipe tranquil boots"], [214,"tranquil boots"], [215,"shadow amulet"], [253,"recipe glimmer cape"], [254,"glimmer cape"], [1021,"river painter"], [1022,"river painter2"], [1023,"river painter3"], [1024,"river painter4"], [1025,"river painter5"], [1026,"river painter6"], [1027,"river painter7"], [1028,"mutation tombstone"], [1029,"super blink"], [1030,"pocket tower"], [1032,"pocket roshan"]])
 
-<<<<<<< HEAD
-=======
-
-
-// // Introduction to FQL - Get Match Data
-// var startTime = Date.now()
-// client.query(
-//     q.Get(
-//         q.Ref(
-//             q.Collection('matches'), 
-//             '4931268777'
-//         )
-//     )
-// ).then(
-//     (ret) => console.log('Match Data: ', ret['data']),
-//     (err) => console.log(err)
-// )
-=======
->>>>>>> app.js update
 // Introduction to FQL - Get Match Data
 var startTime = Date.now()
 client.query(
@@ -44,10 +23,20 @@ client.query(
     (ret) => console.log('Match Data: ', ret['data']),
     (err) => console.log(err)
 )
-<<<<<<< HEAD
-=======
->>>>>>> app.js update
->>>>>>> app.js update
+
+// Introduction to FQL - Get Match Data
+var startTime = Date.now()
+client.query(
+    q.Get(
+        q.Ref(
+            q.Collection('matches'), 
+            '4931268777'
+        )
+    )
+).then(
+    (ret) => console.log('Match Data: ', ret['data']),
+    (err) => console.log(err)
+)
 
 // Easy Business Question 1 - Max match duraion
 client.query(
